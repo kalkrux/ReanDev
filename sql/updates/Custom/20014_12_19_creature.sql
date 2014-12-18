ae3c326a3d51cbacf62d337482f5bde150a2f422
komix <juliogmdev@hotmail.com>
2014-12-18 13:53:38 +0100

    Añadimos el boss en una ubicación en dalaran

---
+++ sql/updates/Custom/20014_12_19_creature.sql
@@ -0,0 +1,2 @@
+INSERT INTO `world`.`creature` (`guid`	`id`	`map`	`spawnMask`	`phaseMask`	`modelid`	`equipment_id`	`position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES
+('210030', '998899',  '571',  '1',  '1',  '0',  '0',  '5901.67',  '646.335',  '645.035',  '5.75076',  '300',  '0',  '0',  '697947264',  '0',  '0',  '0',  '0',  '0');
